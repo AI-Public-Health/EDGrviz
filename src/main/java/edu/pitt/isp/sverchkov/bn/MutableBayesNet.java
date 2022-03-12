5789f7fd267cebefece6f957693d45246b7f336a
Zohar <juj22@pitt.edu>
2022-03-12 14:03:15 -0500

    completed the bn

---
+++ src/main/java/edu/pitt/isp/sverchkov/bn/MutableBayesNet.java
@@ -0,0 +1,18 @@
+package edu.pitt.isp.sverchkov.bn;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author YUS24
+ */
+public interface MutableBayesNet {
+
+    public interface MutableBayesNet<N,V> extends BayesNet<N,V> {
+        void setCPT( N node, Map<N,V> parentAssignment, Map<V,Double> conditionalProbabilities );
+        void addNode( N node, List<V> values );
+        void addArc( N parent, N child );
+        void removeArc(N parent, N child);
+    }
+}
